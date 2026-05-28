a028afb48e309d0868355f85760b7be622a020f4
Ismael <ismaelbaby2006@gmail.com>
2026-05-28 19:58:57 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Router, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -14,7 +15,7 @@ import ProjectYodeck from "./pages/ProjectYodeck";
 
 function AppRouter() {
   return (
-    <Router base="/cyber-portfolio">
+    <Router hook={useHashLocation}>
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/projects/stage" component={ProjectStage} />
